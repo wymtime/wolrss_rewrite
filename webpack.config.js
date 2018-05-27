8b164ad92caeab958c6b7766c8904d78a99d225c
wymtime <mattwyman91@gmail.com>
2018-05-26 21:33:42 -0700

    Add webpack prod env plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -24,5 +25,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx', '*']
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+  ]
 };
